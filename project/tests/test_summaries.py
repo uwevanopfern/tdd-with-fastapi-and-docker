72e206fa56795ee37fcc14c8f6d1587da18a1795
UweVanOpfern <uweaime@gmail.com>
2020-12-17 20:29:33 +0200

    fix: text assertion on test_read_summary test function

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -53,7 +53,7 @@ def test_read_summary(test_app_with_db):
     response_dict = response.json()
     assert response_dict["id"] == summary_id
     assert response_dict["url"] == "https://foo.bar"
-    assert response_dict["summary"] == "Another Great summary, I am telling you"
+    assert response_dict["summary"] == "Another Great summary, Am telling you"
     assert response_dict["created_at"]
 
 
